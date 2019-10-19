0d21d23e854029a6919b5d7d9e16d6fa0cf91c8d
Lucas Limas <llimas.f@gmail.com>
2019-10-19 14:19:12 -0300

    criação do projeto

---
+++ src/view/TelaLivraria.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class TelaLivraria extends JFrame{
+	
+	public TelaLivraria() {
+		setVisible(true);
+		setSize(500, 400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Livraria");
+		
+		
+	}
+}
