76a7c48647180de9a52faaba67f112f6de1a7954
Leonardo Petta do Nascimento <leonardocps9@gmail.com>
2019-10-27 19:35:12 -0300

    Criação de tela excluir autor

--- src/view/TelaExcluiAutor.java
+++ src/view/TelaExcluiAutor.java
@@ -2,37 +2,38 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-import controller.TelaCriaEditoraController;
+import controller.TelaCriaAutorController;
 
-public class TelaCriaEditora extends JFrame{
-	public static JButton btCriarEditora = new JButton("Criar");
+public class TelaExcluiAutor extends JFrame{
+	
+	public static JButton btCriarAutor = new JButton("Criar");
 	public static JTextField tfNome = new JTextField();
-	public static JTextField tfUrl = new JTextField();
+	public static JTextField tfSobreNome = new JTextField();
 	public static JTextField tfId = new JTextField();
 	
-	public TelaCriaEditora() {
+	public TelaExcluiAutor() {
 		setVisible(true);
 		setSize(800, 600);
 		setLayout(new FlowLayout());
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setTitle("Editora");
+		setTitle("Autor");
 		
 		tfNome.setPreferredSize(new Dimension(200,20));
 		add(tfNome);
 		
-		tfUrl.setPreferredSize(new Dimension(200,20));
-		add(tfUrl);
+		tfSobreNome.setPreferredSize(new Dimension(200,20));
+		add(tfSobreNome);
 		
 		tfId.setPreferredSize(new Dimension(100,20));
 		add(tfId);
 		
-		add(btCriarEditora);
-		btCriarEditora.addActionListener(new TelaCriaEditoraController().new onBtCriarEditora());
+		add(btCriarAutor);
+		btCriarAutor.addActionListener(new TelaCriaAutorController().new onBtCriarAutor());
+		
 	}
 }
